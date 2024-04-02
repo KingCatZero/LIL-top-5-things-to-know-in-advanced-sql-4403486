f170d55b1647b0dc50cc0b2591def991daaf36fc
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2024-04-02 17:56:51 +0100

    Update 05.sql

--- Solutions/05.sql
+++ Solutions/05.sql
@@ -1,1 +1,10 @@
-
+SELECT
+	*
+FROM (
+	SELECT
+		*
+		,DENSE_RANK() OVER (PARTITION BY [State] ORDER BY [Registration Date]) AS RegDateRank
+	FROM [dbo].[ConventionAttendees]
+) con
+WHERE
+	RegDateRank < 4

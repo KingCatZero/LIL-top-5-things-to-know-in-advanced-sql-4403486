2c0a93fbf7d80290cfa664a358e33641ab73d75d
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2024-04-02 17:31:07 +0100

    Update 04.sql

--- Solutions/04.sql
+++ Solutions/04.sql
@@ -1,1 +1,19 @@
-
+WITH
+	cteDailyQuantity AS (
+		SELECT
+			OrderDate
+			,SUM(Quantity) AS Quantity
+		FROM [dbo].[OnlineRetailSales]
+		WHERE
+			ProdCategory = 'Drones'
+		GROUP BY
+			OrderDate
+	)
+SELECT
+	*
+	,LAG(Quantity, 1, 0) OVER (ORDER BY OrderDate) AS [Quantity D-1]
+	,LAG(Quantity, 2, 0) OVER (ORDER BY OrderDate) AS [Quantity D-2]
+	,LAG(Quantity, 3, 0) OVER (ORDER BY OrderDate) AS [Quantity D-3]
+	,LAG(Quantity, 4, 0) OVER (ORDER BY OrderDate) AS [Quantity D-4]
+	,LAG(Quantity, 5, 0) OVER (ORDER BY OrderDate) AS [Quantity D-5]
+FROM cteDailyQuantity

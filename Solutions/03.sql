0f6fbd145391fe758b9ae7efb18900196d9a565c
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2024-04-02 16:49:28 +0100

    Update 03.sql

--- Solutions/03.sql
+++ Solutions/03.sql
@@ -1,1 +1,17 @@
-
+SELECT
+	OrderNum
+	,OrderDate
+	,CustName
+	,ProdCategory
+	,ProdName
+	,[Order Total]
+FROM (
+	SELECT
+		*
+		,ROW_NUMBER() OVER (PARTITION BY ProdCategory ORDER BY [Order Total] DESC) AS RowNumber
+	FROM [dbo].[OnlineRetailSales]
+	WHERE
+		CustName = 'Boehm Inc.'
+) AS sales
+WHERE
+	RowNumber < 4
